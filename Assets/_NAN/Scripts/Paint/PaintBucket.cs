32214913520e3ea332a2482bff5d2416f802bd0e
DESKTOP-9K3RR8T\403-07 <kimsu00215@naver.com>
2026-07-21 16:12:49 +0900

    물감 상태를 Paint 폴더로 이동하고 물감통 데이터 모델 추가

---
+++ Assets/_NAN/Scripts/Paint/PaintBucket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 물감통 한 개 단위 직렬화 가능한 클래스
+/// </summary>
+[Serializable]
+public sealed class PaintBucket
+{
+    [SerializeField]
+    private PaintType paintType;
+
+    [SerializeField]
+    [Min(1)]
+    private int range = 1;
+
+    public PaintType PaintType => paintType;
+    public int Range => range;
+}
---
+++ Assets/_NAN/Scripts/Paint/PaintType.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 물감통이 셀에 적용하는 물감의 종류.
+/// Clear는 셀의 모든 RGB 물감 상태를 제거한다.
+/// </summary>
+public enum PaintType
+{
+    Red,
+    Green,
+    Blue,
+    Clear
+}
